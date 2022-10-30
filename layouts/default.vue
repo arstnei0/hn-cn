37c6d56f2a203aff416436d62f7dba2172d0aa18
Zihan Chen <me@zihan.ga>
2022-10-30 03:10:23 +0000

    Init project

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+useHead({
+	title: 'Hacker News',
+	link: [
+		{
+			rel: 'icon',
+			href: '/logo.png'
+		}
+	]
+})
+</script>
+
+<template>
+	<NuxtPage />
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+const stories = await useFetch('/api/top')
+
+console.log(stories)
+</script>
+
+<template>
+
+</template>
